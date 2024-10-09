a930677e76a1f61397de7253e67a9636d4b723da
Desai Manish <138022961+manishdesai17@users.noreply.github.com>
2024-10-09 08:26:20 +0530

    Update ex.py

--- python/ex.py
+++ python/ex.py
@@ -41,7 +41,15 @@ print(x)
 
 s2=""" Gujarat Vidyapith was established as a national university without a
  government charter. Gandhiji manish@gmail.com was its life-long chancellor. Professor
-A T Gidwani was its founder vice-chancellor. After Gandhiji, Sardar 
-Vallabhbhai Patel, Dr. Rajendra Prasad and Morarji Desai adorned the
+A T Gidwani was its founder vice-chancellor.a.manish@gamil.com After Gandhiji, Sardar 
+Vallabhbhai Patel, Dr. m@g.c Rajendra Prasad and Morarji Desai manish123@gmail.c adorned the
 post of chancellor of the Vidyapith.Later, many more institutions, 
 colleges and schools were affiliated to the Vidyapith."""
+
+#abc@gmail.com
+#a@g.c
+p1=r"\w+\@\w+\.\w+"
+p2=r"\w\.\w+\@\w+\.\w+"
+all=p1+"|"+p2
+x=re.findall(all,s2)
+print(x)
